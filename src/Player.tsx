b50a9194f7f71f052c2ecc7552f8d3645380581f
herrKlein <github@kleinwerk.nl>
2023-05-26 16:25:50 +0200

    1 file modified

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
-import { QtiItem } from "@citolab/qti-components/react/qti-item";
-import { qtiTransform } from "@citolab/qti-components/qti-transform";
+import { QtiItem } from '@citolab/qti-components/react/qti-item';
+import { qtiTransform } from '@citolab/qti-components/qti-transform';
 import { ResponseInteraction } from '@citolab/qti-components';
-export const Player = ({items}:{items:{ href: string; identifier: string }[]}) => {
+export const Player = ({
+  items,
+}: {
+  items: { href: string; identifier: string }[];
+}) => {
   const server = '/';
   const pkg = 'assets';
   const [itemIndex, setItemIndex] = useState<number>(0); // the index of the current item
@@ -12,42 +16,57 @@ export const Player = ({items}:{items:{ href: string; identifier: string }[]}) =
 
   useEffect(() => {
     const fetchItem = async () => {
-      const xmlFetch = await fetch(`${server}${pkg}/items/${items[itemIndex]?.href}`);
+      const xmlFetch = await fetch(
+        `${server}${pkg}/items/${items[itemIndex]?.href}`
+      );
       const xmlString = await xmlFetch.text();
       const xml = qtiTransform(xmlString)
-        .assetsLocation(`${server}${pkg}/items/`, ["src", "href", "data"])
-        .pciHooks(`${server}${pkg}/items/`).xml();
+        .assetsLocation(`${server}${pkg}/items/`, ['src', 'href', 'data'])
+        .pciHooks(`${server}${pkg}/items/`)
+        .xml();
       setItemXML(xml);
-    }
+    };
     fetchItem().catch(console.error);
   }, [items, itemIndex]);
 
-  const storeResponse = (itemId: string, response: string | string[], responseIdentifier: string): void => {
+  const storeResponse = (
+    itemId: string,
+    response: string | string[],
+    responseIdentifier: string
+  ): void => {
     const itemdata = itemResponses.current.get(itemId) || [];
-    const iteminteraction = itemResponses.current.get(itemId)?.find(val => val.responseIdentifier === responseIdentifier);
+    const iteminteraction = itemResponses.current
+      .get(itemId)
+      ?.find((val) => val.responseIdentifier === responseIdentifier);
     if (iteminteraction) iteminteraction.response = response;
-    else itemResponses.current.set(itemId, [...itemdata, { response, responseIdentifier }]);
+    else
+      itemResponses.current.set(itemId, [
+        ...itemdata,
+        { response, responseIdentifier },
+      ]);
   };
   const onNext = () => {
-    itemIndex < items.length - 1 && setItemIndex(itemIndex + 1)
-  }
+    itemIndex < items.length - 1 && setItemIndex(itemIndex + 1);
+  };
 
   return (
     <div className="w-screen h-screen bg-gray-100 rounded-2xl flex flex-col items-center justify-center">
       <QtiItem
-      
-        className="w-[800px] h-[600px] bg-white shadow p-4"
+        className="w-[640px] h-[480px] bg-white shadow p-4"
         responses={itemId && itemResponses.current.get(itemId)}
-        qtiinteractionchanged={({ detail }:{detail: any}) => storeResponse(detail.item, detail.response, detail.responseIdentifier)}
+        qtiinteractionchanged={({ detail }: { detail: any }) =>
+          storeResponse(detail.item, detail.response, detail.responseIdentifier)
+        }
         qtioutcomechanged={(e: any) => console.log(e)}
         qtiitemconnected={(e: any) => setItemId(e.detail.identifier)}
         xml={itemXML}
       />
       <div className="flex">
-        <button onClick={() => itemIndex > 0 && setItemIndex(itemIndex - 1)}>vorige</button>
+        <button onClick={() => itemIndex > 0 && setItemIndex(itemIndex - 1)}>
+          vorige
+        </button>
         <button onClick={onNext}>volgende</button>
       </div>
     </div>
-  )
-}
-
+  );
+};
