c6b6b489c24620caf504763f430ae2f410bf0e62
patrick <patrick.deklein@cito.nl>
2023-05-31 15:44:38 +0200

    qti3 items added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import { Player } from "./Player";
+import * as cheerio from 'cheerio';
 
 export const App = () => {
-    const [items, setItems] = useState<{ href: string; identifier: string }[]>([]); // array of item identifiers
+  const [items, setItems] = useState<{ href: string; identifier: string }[]>([]); // array of item identifiers
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const data = await fetch('/assets/items/items.json');
-          const json = await data.json();
-          setItems(json);
-        }
-        fetchData().catch(console.error);
-      }, []);
-      
-    return(<Player items={items}></Player>)
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('/assets/items/imsmanifest.xml');
+      const xml = await data.text();
+
+      const $ = cheerio.load(xml);
+
+      const resources: { href: string; identifier: string }[] = [];
+      $('resource').each((_, element) => {
+        const identifier = $(element).attr('identifier')!;
+        const href = $(element).attr('href')!;
+
+        resources.push({ identifier, href });
+      });
+
+      setItems(resources)
+
+    }
+    fetchData().catch(console.error);
+  }, []);
+
+  return (<Player items={items}></Player>)
 }
 
 
